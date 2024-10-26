d84c880384001266f8d5107b4321aecb2a16722b
Agustin-del <eddyruveda@gmail.com>
2024-10-25 22:12:24 -0300

    get archivo html

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 import Fastify from 'fastify';
+import path from 'path';
+import fastifyStatic from '@fastify/static';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({
     logger: true,
 });
 
-fastify.get('/', function(request, reply) {
-    reply.send({hello:'world'});
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'static'),
+    prefix: '/'
 })
 
-fastify.listen({port: 3000}, function(err, address) {
-    if (err) {
-        fastify.log.error(err);
+fastify.get('/', async function(request, reply) {
+    return reply.sendFile('index.html');
+})
+
+const start = async () => {
+    try {
+        await fastify.listen({port: 3000});
+    } catch (e) {
+        fastify.log.error(e);
         process.exit(1);
     }
-})
+}
+
+start()
